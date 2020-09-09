464d41cb648ea668ffb99f91c4fad6cd79a0f97e
Anette <anetteagura@gmail.com>
2020-09-09 11:35:52 +0300

    Lisatud model ja paar rida mvc_ui.

---
+++ model.py
@@ -0,0 +1,28 @@
+import helpers
+
+
+class Model:
+
+    def __init__(self, elemendid):
+        self.lisa_elemendid(elemendid)
+
+    def lisa_elemendid(self, elemendid):
+        helpers.lisa_elemendid(elemendid)
+
+    def lisa_element(self, nimetus, hind, kogus):
+        helpers.lisa_element(nimetus, hind, kogus)
+
+    def loe_elemendid(self):
+        return helpers.loe_elemendid()
+
+    def loe_element(self, nimetus):
+        return helpers.loe_element(nimetus)
+
+    def uuenda_element(self, nimetus, hind, kogus):
+        helpers.uuenda_element(nimetus, hind, kogus)
+
+    def kustuta_element(self, nimetus):
+        helpers.kustuta_element(nimetus)
+
+    def kustuta_koik(self):
+        helpers.kustuta_koik()
